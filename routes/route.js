0fb66885c6be945fe67e2f42be5018320d89bdc6
Juana-Tut <2023159062@ub.edu.bz>
2025-04-07 11:15:10 -0600

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -20,7 +20,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/api/picture", (req, res) => {
-  fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  fetch('https://api.nasa.gov/planetary/apod?api_key=YhhCbdfY7uDyqOMT1bzBUuRbGCUOg1N1c2wAvbIC&count=1')
     .then((response) => {
       if (!response.ok) {
         throw new Error('NASA API request failed');
@@ -29,7 +29,7 @@ router.get("/api/picture", (req, res) => {
     })
     .then((data) => {
       const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-      res.json({ data, greeting: randomGreeting });
+      res.json({ data: data[0], greeting: randomGreeting });
     })
     .catch((error) => {
       console.error('Error fetching NASA APOD:', error);
